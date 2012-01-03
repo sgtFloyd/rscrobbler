ed0eb199186b21353065d56e8dc404b0e480a803
Gabe Smith <gabe@paperlesspost.com>
2012-01-03 18:36:29 -0500

    Complete documentation on LastFM::Geo

--- lib/lastfm/geo.rb
+++ lib/lastfm/geo.rb
@@ -6,6 +6,12 @@ module LastFM
 
       # Get all events in a specific location by country or city name.
       #
+      # @option params [String, optional] :location location to retrieve events for
+      # @option params [String, optional] :lat      latitude value to retrieve events for
+      # @option params [String, optional] :long     longitude value to retrieve events for
+      # @option params [Fixnum, optional] :distance find events within a specified radius (in kilometres)
+      # @option params [Fixnum, optional] :page     the page number to fetch. defaults to first page
+      # @option params [Fixnum, optional] :limit    the number of results to fetch per page. defaults to 50
       # @see http://www.last.fm/api/show?service=270
       def get_events( params )
         LastFM.get( "#{TYPE}.getEvents", !:secure, params )
@@ -13,6 +19,10 @@ module LastFM
 
       # Get a chart of artists for a metro.
       #
+      # @option params [String, required] :country  a country name, as defined by ISO 3166-1
+      # @option params [String, required] :metro    the metro's name
+      # @option params [String, optional] :start    beginning timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
+      # @option params [String, optional] :end      ending timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
       # @see http://www.last.fm/api/show?service=421
       def get_metro_artist_chart( params )
         LastFM.get( "#{TYPE}.getMetroArtistChart", params )
@@ -20,6 +30,10 @@ module LastFM
 
       # Get a chart of hyped (up and coming) artists for a metro.
       #
+      # @option params [String, required] :country  a country name, as defined by ISO 3166-1
+      # @option params [String, required] :metro    the metro's name
+      # @option params [String, optional] :start    beginning timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
+      # @option params [String, optional] :end      ending timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
       # @see http://www.last.fm/api/show?service=420
       def get_metro_hype_artist_chart( params )
         LastFM.get( "#{TYPE}.getMetroHypeArtistChart", params )
@@ -27,6 +41,10 @@ module LastFM
 
       # Get a chart of hyped (up and coming) tracks for a metro.
       #
+      # @option params [String, required] :country  a country name, as defined by ISO 3166-1
+      # @option params [String, required] :metro    the metro's name
+      # @option params [String, optional] :start    beginning timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
+      # @option params [String, optional] :end      ending timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
       # @see http://www.last.fm/api/show?service=422
       def get_metro_hype_track_chart( params )
         LastFM.get( "#{TYPE}.getMetroHypeTrackChart", params )
@@ -34,6 +52,10 @@ module LastFM
 
       # Get a chart of tracks for a metro.
       #
+      # @option params [String, required] :country  a country name, as defined by ISO 3166-1
+      # @option params [String, required] :metro    the metro's name
+      # @option params [String, optional] :start    beginning timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
+      # @option params [String, optional] :end      ending timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
       # @see http://www.last.fm/api/show?service=423
       def get_metro_track_chart( params )
         LastFM.get( "#{TYPE}.getMetroTrackChart", params )
@@ -41,6 +63,10 @@ module LastFM
 
       # Get a chart of the artists which make that metro unique
       #
+      # @option params [String, required] :country  a country name, as defined by ISO 3166-1
+      # @option params [String, required] :metro    the metro's name
+      # @option params [String, optional] :start    beginning timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
+      # @option params [String, optional] :end      ending timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
       # @see http://www.last.fm/api/show?service=424
       def get_metro_unique_artist_chart( params )
         LastFM.get( "#{TYPE}.getMetroUniqueArtistChart", params )
@@ -48,6 +74,10 @@ module LastFM
 
       # Get a chart of the tracks which make that metro unique
       #
+      # @option params [String, required] :country  a country name, as defined by ISO 3166-1
+      # @option params [String, required] :metro    the metro's name
+      # @option params [String, optional] :start    beginning timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
+      # @option params [String, optional] :end      ending timestamp of the weekly range requested (see: Geo.get_weekly_chart_list)
       # @see http://www.last.fm/api/show?service=425
       def get_metro_unique_track_chart( params )
         LastFM.get( "#{TYPE}.getMetroUniqueTrackChart", params )
@@ -55,6 +85,7 @@ module LastFM
 
       # Get a list of available chart periods for this metro, expressed as date ranges which can be sent to the chart services.
       #
+      # @option params [String, required] :metro    metro name to fetch the charts list for
       # @see http://www.last.fm/api/show?service=426
       def get_metro_weekly_chartlist( params )
         LastFM.get( "#{TYPE}.getMetroWeeklyChartlist", params )
@@ -62,6 +93,7 @@ module LastFM
 
       # Get a list of valid countries and metros for use in the other webservices
       #
+      # @option params [String, optional] :country  restrict results to metros from a particular country, as defined by ISO 3166-1
       # @see http://www.last.fm/api/show?service=435
       def get_metros( params )
         LastFM.get( "#{TYPE}.getMetros", params )
@@ -69,6 +101,9 @@ module LastFM
 
       # Get the most popular artists on Last.fm by country
       #
+      # @option params [String, required] :country  a country name, as defined by ISO 3166-1
+      # @option params [Fixnum, optional] :page     the page number to fetch. defaults to first page
+      # @option params [Fixnum, optional] :limit    the number of results to fetch per page. defaults to 50
       # @see http://www.last.fm/api/show?service=297
       def get_top_artists( params )
         LastFM.get( "#{TYPE}.getTopArtists", params )
@@ -76,6 +111,10 @@ module LastFM
 
       # Get the most popular tracks on Last.fm by country
       #
+      # @option params [String, required] :country  a country name, as defined by ISO 3166-1
+      # @option params [String, optional] :location location to fetch the charts for
+      # @option params [Fixnum, optional] :page     the page number to fetch. defaults to first page
+      # @option params [Fixnum, optional] :limit    the number of results to fetch per page. defaults to 50
       # @see http://www.last.fm/api/show?service=298
       def get_top_tracks( params )
         LastFM.get( "#{TYPE}.getTopTracks", params )
