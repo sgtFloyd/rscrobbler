1d645a9e022f54a61afb3fe86dbc0dc0830c937c
Gabe Smith <gabe@paperlesspost.com>
2012-01-04 16:29:12 -0500

    Fix YARD tags

--- lib/lastfm/radio.rb
+++ lib/lastfm/radio.rb
@@ -4,18 +4,18 @@ module LastFM
 
       TYPE = 'radio'
 
-      # see: http://www.last.fm/api/show?service=256
+      # @see: http://www.last.fm/api/show?service=256
       def get_playlist( params )
         LastFM.requires_authentication
         LastFM.get( "#{TYPE}.getPlaylist", params, :secure )
       end
 
-      # see: http://www.last.fm/api/show?service=418
+      # @see: http://www.last.fm/api/show?service=418
       def search( params )
         LastFM.get( "#{TYPE}.search", params )
       end
 
-      # see: http://www.last.fm/api/show?service=160
+      # @see: http://www.last.fm/api/show?service=160
       def tune( params )
         LastFM.requires_authentication
         LastFM.post( "#{TYPE}.tune", params )
